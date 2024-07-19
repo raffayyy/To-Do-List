73b011d5088085992304628ae111bf47b6a15e10
raffayyy <muhammadrafay5165@gmail.com>
2024-07-19 19:18:36 +0500

    Done, remove and clear buttons added with functionality

--- script.js
+++ script.js
@@ -9,11 +9,35 @@ function addToDo() {
   let content = document.createTextNode(input);
   li.appendChild(content);
 
+  let done = document.createElement("button");
+  done.innerHTML = "Done";
+  done.className = "donebtn";
+
+  let remove = document.createElement("button");
+  remove.innerHTML = "Remove";
+  remove.className = "removebtn";
+
+  let span = document.createElement("span");
+  span.className = "badge";
+  span.appendChild(done);
+  span.appendChild(remove);
+
+  li.appendChild(span);
   document.getElementById("taskList").appendChild(li);
-  console.log(input);
 
   document.getElementById("taskInput").value = "";
-}
 
+  done.addEventListener("click", function () {
+    li.style.textDecoration = "line-through";
+  });
+
+  remove.addEventListener("click", function () {
+    li.style.display = "none";
+  });
+}
+function clearList() {
+  document.getElementById("taskList").innerHTML = "";
+}
+document.getElementById("clear").addEventListener("click", clearList);
 document.getElementById("addTaskButton").addEventListener("click", addToDo);
 // window.onload
